a79425f8b22177501056f67ebc0716d8fb7730b6
GovindiTharshika <govindiedirisinghe@gmail.com>
2026-06-09 14:46:27 +0530

    feat: initialize Express server with Prisma database integration and middleware support

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,15 @@
-const mongoose = require("mongoose");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-
-    console.log(
-      `MongoDB Connected: ${conn.connection.host}`
-    );
+    await prisma.$connect();
+    console.log("MongoDB Connected via Prisma");
   } catch (error) {
     console.log(error);
     process.exit(1);
   }
 };
 
-module.exports = connectDB;
+module.exports = { prisma, connectDB };
